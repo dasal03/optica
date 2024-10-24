3010d94846efbb707d4c1b087c5a48c0077a16f7
Diego Andrés Saltarín <miguelan238@gmail.com>
2024-10-24 18:25:25 -0500

    Get medical history API

--- Classes/History.py
+++ Classes/History.py
@@ -1,6 +1,8 @@
 import os
 from sqlalchemy import select, and_
 from sqlalchemy.sql.functions import concat
+from Utils.ExceptionsTools import CustomException
+from Utils.GeneralTools import get_input_data
 from Utils.Validations import Validations
 from Models.User import UserModel
 from Models.History import HistoryModel
@@ -11,27 +13,31 @@ class History:
     """Class for manage clinical history."""
     def __init__(self, db):
         self.db = db
-        self.validations = Validations()
+        self.validations = Validations(self.db)
 
-    def search_history(self, data):
+    def search_history(self, event):
         """Get all clinical history for a specific document."""
+        request = get_input_data(event)
+        document_type_id = request.get("document_type_id", 0)
+        document_number = request.get("document_number", 0)
+        base_dir = os.path.abspath("temp")
 
-        validation_errors = self.validations.validate(
-            self.validations.param(
-                "document_number", data["document_number"], int),
-            self.validations.param(
-                "document_type_id", data["document_type_id"], int)
-        )
+        validation_list = [
+            self.validations.param("document_type_id", int, document_type_id),
+            self.validations.param("document_number", int, document_number)
+        ]
 
-        if validation_errors:
-            raise AssertionError(validation_errors)
+        validated = self.validations.validate(validation_list, cast=True)
+
+        if not validated["isValid"]:
+            raise CustomException(validated["data"])
 
         stmt = (
             select(
                 UserModel.user_id,
-                concat(
-                    UserModel.first_name, " ", UserModel.last_name
-                ).label("full_name"),
+                concat(UserModel.first_name, " ", UserModel.last_name).label(
+                    "full_name"
+                ),
                 HistoryModel.file_url,
                 HistoryModel.created_at,
                 HistoryModel.updated_at,
@@ -39,23 +45,38 @@ class History:
             .join(HistoryModel, HistoryModel.user_id == UserModel.user_id)
             .join(
                 DocumentTypeModel,
-                UserModel.document_type_id ==
-                DocumentTypeModel.document_type_id
-            )
-            .filter(
-                UserModel.document_number == data["document_number"],
                 and_(
-                    UserModel.document_type_id == data["document_type_id"],
+                    UserModel.document_type_id ==
+                    DocumentTypeModel.document_type_id,
                     DocumentTypeModel.active == 1
-                )
+                ),
+            )
+            .where(
+                UserModel.document_number == document_number,
+                UserModel.document_type_id == document_type_id
             )
         )
         results = self.db.query(stmt).as_dict()
-        base_dir = os.path.abspath("temp")
 
-        for result in results:
-            # Build absolute path
-            file_name = result["file_url"]
-            result["file_url"] = os.path.join(base_dir, f"{file_name}.pdf")
+        if results:
+            for result in results:
+                # Build absolute path
+                file_name = result["file_url"]
+                result["file_url"] = os.path.join(base_dir, f"{file_name}.pdf")
 
-        return results if results else []
+            status_code = 200
+            data = results
+        else:
+            status_code = 404
+            data = []
+
+        return {"statusCode": status_code, "data": data}
+
+    def create_history(self, data):
+        pass
+
+    def update_history(self, data):
+        pass
+
+    def delete_history(self, data):
+        pass
